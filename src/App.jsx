dc70306911e893c963837d1ce4f7c0cc1c7e4b91
vinicius tenedini <tenedini04rt@gmail.com>
2025-04-04 12:10:48 -0300

    projeto

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,47 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+        <>
+        
+    <div className="caixa1">
+        <img src="https://img.icons8.com/?size=100&id=ocmaRApBZ0nV&format=png&color=000000" alt=""/>
+    </div>
+    <div className="container">
+        <div className="gridprodutos">
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/30/33/15/303315cd9d75c9ad4e12a5a507553098.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/7e/3d/6a/7e3d6a347a5d9399a0a2d6eeaeb57aa7.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/ae/0a/a1/ae0aa1dce2a9e8f1d26b09f67a335e05.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/f6/b9/08/f6b9081732e03dd9873edf7c3fabcf34.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/d8/19/af/d819af9043abf559d3c6290f6eca57ca.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/40/e9/58/40e958f6dc59e3618a5878f15b045461.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/e4/66/ce/e466ce955785fbc5eaf3eb3210b03b35.jpg" alt="" className="produtos"/>
+            </div>
+            <div className="produtos">
+                <img src="https://i.pinimg.com/236x/95/c2/c1/95c2c1d749a846b8d7055dab68a97d96.jpg" alt="" className="produtos"/>
+            </div>
+        </div>
+        
+    </div>
+    <div className="caixa2">
+        <img src="https://img.icons8.com/?size=100&id=ocmaRApBZ0nV&format=png&color=000000" alt=""/>
+    </div>
+
+
+        </>    
   )
 }
 
